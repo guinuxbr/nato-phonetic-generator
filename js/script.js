7840a4a13803f16026d227ed426ec216388319dd
GuinuxBR <guinuxbr@gmail.com>
2024-03-26 14:47:58 +0000

    Added .gitignore; Added a 'Clear' button

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
 document.querySelector("#text-to-convert").value = "";
 document.querySelector("#converted-text-p").value = "";
 let convertBtn = document.querySelector("#convert-btn");
+let clearBtn = document.querySelector("#clear-btn");
 let modalMessage = document.querySelector("#modal-message");
 let modalClose = document.querySelector("#modal-close");
 
 let alphabet = {
-  a: "alpha",
-  b: "bravo",
-  c: "charlie",
-  d: "delta",
-  e: "echo",
-  f: "foxtrot",
-  g: "golf",
-  h: "hotel",
-  i: "india",
-  j: "juliet",
-  k: "kilo",
-  l: "lima",
-  m: "mike",
-  n: "november",
-  o: "oscar",
-  p: "papa",
-  q: "quebec",
-  r: "romeo",
-  s: "sierra",
-  t: "tango",
-  u: "uniform",
-  v: "victor",
-  w: "whisky",
-  x: "xray",
-  y: "yankee",
-  z: "zulu",
+  "a": "alpha",
+  "b": "bravo",
+  "c": "charlie",
+  "d": "delta",
+  "e": "echo",
+  "f": "foxtrot",
+  "g": "golf",
+  "h": "hotel",
+  "i": "india",
+  "j": "juliet",
+  "k": "kilo",
+  "l": "lima",
+  "m": "mike",
+  "n": "november",
+  "o": "oscar",
+  "p": "papa",
+  "q": "quebec",
+  "r": "romeo",
+  "s": "sierra",
+  "t": "tango",
+  "u": "uniform",
+  "v": "victor",
+  "w": "whisky",
+  "x": "xray",
+  "y": "yankee",
+  "z": "zulu",
 };
 
 convertBtn.addEventListener("click", convert);
+clearBtn.addEventListener("click", clear);
 
 modalClose.addEventListener("click", function() {
     modalMessage.classList.remove("is-active")  
@@ -59,9 +61,23 @@ function convert() {
                 convertedText.innerHTML += `${firstLetter}${alphabet[letter].slice(1)} `;
             } else if (letter === " ") {
                 convertedText.innerHTML += ` (space) `;
+            } else if (letter === "@") {
+                convertedText.innerHTML += ` (at) `;
+            } else if (letter === "-") {
+                convertedText.innerHTML += ` (hyphen) `;
+            } else if (letter === "_") {
+                convertedText.innerHTML += ` (underscore) `;
+            } else if (letter === ".") {
+                convertedText.innerHTML += ` (dot) `;
             } else {
                 convertedText.innerHTML += `${letter}`;
             }
         }
     }
+}
+
+
+function clear() {
+    // Clear the text fields by refreshing the page
+    window.location.reload();
 }
